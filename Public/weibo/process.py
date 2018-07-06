0426fd8b9368a1435f17fa52331a5df167e9763e
hangtingchen <465244243@qq.com>
2018-07-06 09:47:41 +0800

    fix bugs

--- Public/weibo/process.py
+++ Public/weibo/process.py
@@ -12,73 +12,76 @@ import sys
 import time
 
 pattern2enter=[
-    r'\xa3\xac',r',',
-    r'\xa3\xa1',r'!',
-    r'…',
-    r'[^\d].[^\d]',r'\xa1\xa3',r'[^\d]\xa3\xae[\^d]',
-    r'\xa3\xba',r':',
-    r'\xa1\xb0',r'\xa1\xb1',r'\'',
-    r'\xa3\xa8',r'\xa3\xa9',r'\(',r'\)',
-    r'\xa1\xab',r'~',
-    r'/',
-    r'\xa3\xba',r':',
-    r'\?',r'\xa3\xbf',
-    r'\xa1\xa2',
+    r'//',
+    # '@霍思燕 '
+    r'@[^ :：。\.]+ ',r'@[^ :：。\.]+:',r'@[^ :：。\.]+：',r'@[^ :：。\.]+。',r'@[^ :：。\.]+\.',r'@[^ :：。\.]+'
 ]
 pattern2blank=[
     # '[开心]'
-    r'\[.*\]',r'\xa1\xbe.*\xa1\xbf',
-    # '@霍思燕 '
-    r'@.* ',r'@.*:',r'@.*\xa3\xba',
+    r'\[[^\]]+\]',r'【[^】]+】',
     # '#Sweet Morning#'
     r'#.*#',
+    r'http://[a-zA-Z0-9/\.]+'
     # '《名流巨星》'
-    r'\xa1b6.*\xa1b7',
+    # r'\xa1b6.*\xa1b7',
 ]
 pattern2pattern=[
-    ('0','\xa3\xb0'),
-    ('1','\xa3\xb1'),
-    ('2','\xa3\xb2'),
-    ('3','\xa3\xb3'),
-    ('4','\xa3\xb4'),
-    ('5','\xa3\xb5'),
-    ('6','\xa3\xb6'),
-    ('7','\xa3\xb7'),
-    ('8','\xa3\xb8'),
-    ('9','\xa3\xb9'),
-    (r'.','\xa3\xae'),
+    (r'0','０'),
+    (r'1','１'),
+    (r'2','２'),
+    (r'3','３'),
+    (r'4','４'),
+    (r'5','５'),
+    (r'6','６'),
+    (r'7','７'),
+    (r'8','８'),
+    (r'9','９'),
+    (r',','，'),
+    (r'!','！'),
+    (r'\.','。'),
+    (r':','：'),
+    # 瞎几把判断
+    # (r'\'','“'),
+    (r'\(','（'),
+    (r'\)','）'),
+    (r'~','～'),
+    (r'/','／'),
+    (r'\?','？'),
 ]
 # pattern2space=[]
 
 def loadCorpus(line,showFlag=False):
-    if(showFlag):print(line)
-    for p in pattern2pattern:
-        line=re.sub(p[0],p[1],line)
+    line=line.strip()
+    print(line)
+    # 替换数字中的小数点为全角
+    line=re.sub(r'(?P<d1>\d+)\.(?P<d2>\d+)','\g<d1>．\g<d2>',line)
     for p in pattern2blank:
         line=re.sub(p,'',line)
     for p in pattern2enter:
         line=re.sub(p,'\n',line)
-
-    line=re.sub('[a-zA-Z]','',line)
+    for p in pattern2pattern:
+        line=re.sub(p[0],p[1],line)
+    line=re.sub(r'([a-zA-Z][a-zA-Z]+[a-zA-Z，！。：（）～／？])?','',line)
+    # line=re.sub(r'[a-zA-Z]','',line)
     line=re.sub(' ','',line)
 
     line=line.split('\n')
-    line=[l for l in line if len(l)!=0]
+    line=[l for l in line if len(l)>0]
 
-    if(showFlag):
-        time.sleep(5)
-        print(line)
+    if(showFlag):time.sleep(3)
+    print(line)
+    print('\n')
     return line
 
 def writeCorpus(lineList,f):
     for line in lineList:
         if(len(line)<=2):return
-        else:f.writeline(' '.join(line)+'\n')
+        else:f.write(' '.join(line)+'\n')
 
 if __name__ == '__main__':
     filefrom = sys.argv[1]
     fileto=sys.argv[2]
-    showFlag=True
+    showFlag=False
     with open(filefrom,'r') as ffrom, open(fileto,'w') as fto:
         for line in ffrom:
             lineList=loadCorpus(line,showFlag=showFlag)
